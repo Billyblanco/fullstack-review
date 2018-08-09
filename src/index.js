1d62c633af099381fdd1c33a33815d2e978fe52b
Billyblanco <bayleenoe@gmail.com>
2018-08-09 12:29:08 -0600

    REDUX

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './redux/store'
+import { Provider } from 'react-redux'
+// import { HashRouter, Route } from 'react-router-dom'
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+<Provider store={store}>
+  <App />
+</Provider>
+
+, document.getElementById('root'));
 // registerServiceWorker();
